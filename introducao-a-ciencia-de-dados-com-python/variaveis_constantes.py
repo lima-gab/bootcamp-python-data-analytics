fb04203971d6cd8faf5a1c34504cfe5805b0dd9a
lima-gab <lima.gab@outlook.com>
2024-03-13 21:23:13 -0300

    conclusão de aula

---
+++ introducao-a-ciencia-de-dados-com-python/variaveis_constantes.py
@@ -0,0 +1,12 @@
+nome = 'Gabriel'
+idade = 22
+
+nome, idade = 'Giovana', 21
+
+print(nome, idade)
+
+limite_saque_diario = 1000
+
+BRAZILIAN_STATES = ['SP', 'RJ', 'SC', 'RS']
+
+print(BRAZILIAN_STATES)
