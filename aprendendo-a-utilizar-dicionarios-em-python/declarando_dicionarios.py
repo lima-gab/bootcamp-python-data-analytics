6ed1b83352f962e1790154aec4af31a9d0020f30
lima-gab <lima.gab@outlook.com>
2024-03-27 22:06:12 -0300

    conclusão de módulo

--- aprendendo-a-utilizar-dicionarios-em-python/declarando_dicionarios.py
+++
@@ -1,5 +0,0 @@
-pessoa = {'nome': 'Gabriel', 'idade': 22}
-
-pessoa = dict(nome='Gabriel', idade=22)
-
-pessoa['telefone'] = '1234-5678' # ['nome': 'Gabriel', 'idade': 22, 'telefone': '1234-5678']
--- aprendendo-a-utilizar-dicionarios-em-python/metodo_items.py
+++
@@ -1,4 +0,0 @@
-contatos = {"gabriel@gmail.com": {"nome": "Gabriel", "telefone": "3333-2221"}}
-
-resultado = contatos.items()  # dict_items([('gabriel@gmail.com', {'nome': 'Gabriel', 'telefone': '3333-2221'})])
-print(resultado)
